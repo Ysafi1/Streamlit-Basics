00decc25c98219f347b9e7eb9c43428b2168c9c0
Ysafi1 <kabandasafi18@gmail.com>
2023-09-25 13:29:05 +0200

    Init streamlit app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.write("""#My interface
-         This is my first interface using streamlit
+st.write("""# My interface
+         This is my first interface using streamlit.
          """)
