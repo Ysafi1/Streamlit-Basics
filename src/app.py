1463b4b68b2e495a0f02d9ed782a0afe2f53ceed
Ysafi1 <kabandasafi18@gmail.com>
2023-09-25 17:16:57 +0200

    Init streamlit app

--- src/app.py
+++ src/app.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
 
 st.write("""# My interface
          This is my first interface using streamlit.
          """)
+
+sepal_height =st.number_input("Enter sepal height")
+sepal_width = st.number_input("Enter sepal width")
+
+petal_height = st.number_input("Enter petal height")
+petal_width = st.number_input("Enter petal width")
+
+if st.button("predict"):
+    st.text(f"The iris has been classified as : '{''}' .")
